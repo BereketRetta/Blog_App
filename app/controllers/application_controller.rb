12ba62b69b4dea2b2cac5e0f39905b544d3d81ab
Bereket Adego Retta <bereket@etmsoftwareplc.com>
2022-05-26 13:08:40 +0300

    add controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-
-  before_action :update_allowed_parameters, if: :devise_controller?
-
-  protected
-
-  def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) do |u|
-      u.permit(:name, :email, :password, :email_confirmation, :password_confirmation)
-    end
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
+  def current_user
+    User.first
   end
 end
