44c3201155a4093b35c8529e07df53d91239044c
Bereket Adego Retta <bereket@etmsoftwareplc.com>
2022-05-26 08:36:15 +0300

    db migrate done

--- db/migrate/20220511133212_create_posts.rb
+++ db/migrate/20220511133212_create_posts.rb
@@ -1,11 +1,8 @@
-class CreatePosts < ActiveRecord::Migration[7.0]
+class CreateLikes < ActiveRecord::Migration[7.0]
   def change
-    create_table :posts do |t|
-      t.string :title
-      t.text :text
-      t.integer :comments_counter
-      t.integer :likes_counter
+    create_table :likes do |t|
       t.references :user, null: false, foreign_key: true
+      t.references :post, null: false, foreign_key: true
 
       t.timestamps
     end
