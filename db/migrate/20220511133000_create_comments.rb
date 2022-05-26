d1ac6c96c6902ee0374478f770ea8bac3a8060f7
Bereket Adego Retta <bereket@etmsoftwareplc.com>
2022-05-26 08:38:00 +0300

    migration complete

--- db/migrate/20220511133000_create_comments.rb
+++ db/migrate/20220511133000_create_comments.rb
@@ -1,9 +1,11 @@
-class CreateComments < ActiveRecord::Migration[7.0]
+class CreatePosts < ActiveRecord::Migration[7.0]
   def change
-    create_table :comments do |t|
+    create_table :posts do |t|
+      t.string :title
       t.text :text
+      t.integer :comments_counter
+      t.integer :likes_counter
       t.references :user, null: false, foreign_key: true
-      t.references :post, null: false, foreign_key: true
 
       t.timestamps
     end
